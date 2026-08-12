58a6214ba721376ccfbed9e1fcd0237c8924ada9
Shiny <hide771223@gmail.com>
2026-08-12 18:06:27 +0800

    版本號升至 1.2.24：彩蛋熊熊寬度貼齊登入卡片、高度自然拉高、副標改字
    熊熊寬度改為與登入卡片一致（400px／手機 100vw-28px），高度照圖片
    941:1672 比例自然拉高以吃回圖片留白；副標改為「路易斯貼在螢幕上瞪你」。

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -113,7 +113,7 @@ export default function Login() {
             <img className="jump-scare__bear-img" src={louisPng} alt="熊熊" />
           </div>
           <p className="jump-scare__msg">你想盜我帳號嗎!!!</p>
-          <p className="jump-scare__sub">熊熊整隻貼在你螢幕上瞪你</p>
+          <p className="jump-scare__sub">路易斯貼在螢幕上瞪你</p>
           <span className="jump-scare__dismiss">點擊畫面關閉</span>
         </div>
       )}
